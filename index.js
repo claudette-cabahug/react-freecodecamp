9f09051d540c41dd65d42120177485b46c0f5df5
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-05-13 12:18:32 +1200

    add dependencies files

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-function Navbar() {
-  return (
-    <nav>
-      <div>Navbar</div>
-    </nav>
-  )
-}
+// function Navbar() {
+//   return (
+//     <nav>
+//       <div>Navbar</div>
+//     </nav>
+//   )
+// }
 
-function MainContent() {
-  return (
-    <h1>I'm learning React!</h1>
-  )
-}
+// function MainContent() {
+//   return (
+//     <h1>I'm learning React!</h1>
+//   )
+// }
 
 
-ReactDOM.render(
-  <div>
-    {/* concept of composability */}
-    <Navbar />
-    <MainContent />
-  </div>, 
-    document.getElementById("root"))
+// ReactDOM.render(
+//   <div>
+//     {/* concept of composability */}
+//     <Navbar />
+//     <MainContent />
+//   </div>, 
+//     document.getElementById("root"))
+
+const h1 =document.createElement("h1")
+h1.textContent = "Hello World"
+h1.className = "header"
+console.log(h1)
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+
+ReactDOM.render(element, document.getElementsByID("root"))
